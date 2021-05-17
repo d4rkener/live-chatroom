c71423e0a5e3f5f2c4eea6d99c4375215890cef2
Rahul Shaw <shawrahul2006@gmail.com>
2021-05-17 19:13:35 +0530

    Navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there... display name here</p>
+      <p class="email">Currently logged in as... email</p>
+    </div>
+    <button @click="handleClick">Logout</button>
+  </nav>
+</template>
+
+<script>
+import useLogout from '../composables/useLogout'
+
+export default {
+  name: 'Navbar',
+  setup() {
+    const { error, logout } = useLogout()
+
+    const handleClick = async () => {
+      await logout()
+      if (!error.value) {
+        console.log('user logged out')
+      }
+    }
+
+    return { handleClick }
+  },
+}
+</script>
+
+<style>
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,12 +1,33 @@
 <template>
-  <p>Chatroom</p>
+  <div class="container">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
+
 export default {
   name: 'Chatroom',
+  components: { Navbar },
 }
 </script>
 
 <style>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
 </style>
